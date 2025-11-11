dc3ff10ca23cdb3175ad48f5deee3a687d5b4a6f
akycode08 <akylbekkarim01@gmail.com>
2025-11-10 18:54:50 -0600

    Create RampController.cs

---
+++ Services/Controllers/RampController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Alicat.Services.Protocol;
+using Alicat.Services.Serial;
+
+namespace Alicat.Services.Controllers
+{
+    public sealed class RampController
+    {
+        private readonly SerialClient _serial;
+
+        public RampController(SerialClient serial) => _serial = serial;
+
+        /// <summary>
+        /// Устанавливает скорость рампа SR без каких-либо конвертаций.
+        /// Возвращает true, если команда отправлена.
+        /// </summary>
+        public bool TryApply(double? pressureRamp)
+        {
+            if (_serial is null || pressureRamp is null) return false;
+
+            double r = pressureRamp.Value;
+            if (r < 0) r = 0;                // простая защита
+            var s = r.ToString("G", CultureInfo.InvariantCulture);
+
+            _serial.Send(AlicatCommands.SetRamp(s));
+            return true;
+        }
+    }
+}
